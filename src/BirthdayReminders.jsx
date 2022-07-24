31539ee930ab51d84bb1d034a74cae37686e3d42
Daniel Pytel <danielrpytel@gmail.com>
2022-07-23 21:34:09 -0500

    initial commit

---
+++ src/BirthdayReminders.jsx
@@ -0,0 +1,9 @@
+
+function BirthdayReminders() {
+  return (
+    <div className="h-screen w-screen bg-gray-800">
+    </div>
+    );
+}
+
+export default BirthdayReminders;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import BirthdayReminders from './BirthdayReminders';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BirthdayReminders />
   </React.StrictMode>
 );
 
